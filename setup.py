cc8d78d980804496b94f8a60f76573c9de6aa13c
Ross J. Reedstrom <reedstrm@cnx.org>
2011-08-19 20:42:01 +0000

    fixup setup.py for dynamic version num

--- setup.py
+++ setup.py
@@ -1,6 +1,44 @@
 from setuptools import setup, find_packages
 import os
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read("Products", "RhaptosPatchTool", "version.txt").strip()
+
+setup(name='Products.RhaptosPatchTool',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Rhaptos dev team',
+      author_email='rhaptos@cnx.org',
+      url='http://rhaptos.org',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      tests_require = [
+           'zope.testing>=3.5',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
+from setuptools import setup, find_packages
+import os
+
 version = '0.7.1'
 
 setup(name='Products.RhaptosPatchTool',
